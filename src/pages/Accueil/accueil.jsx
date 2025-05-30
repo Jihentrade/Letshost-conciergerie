02e9266bd5fd5d78818c38b9b1fbc7cafd3b60f9
Jihentrade <jihen781227@gmail.com>
2025-05-30 11:47:03 +0100

    acceuil video

--- src/pages/Accueil/accueil.jsx
+++ src/pages/Accueil/accueil.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import coverImage from "../../../src/assets/conciergerie-nice-couverture.jpg";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Footer from "../../components/footer";
@@ -11,128 +10,38 @@ import FAQSection from "../../components/faqSection";
 import Service from "../../components/Serviice";
 import LogoServ from "../../components/leslogo";
 import { Helmet } from "react-helmet";
-import image1 from "../../assets/Acc1.png";
-import image2 from "../../assets/Acc2.png";
-import image3 from "../../assets/Acc3.png";
-import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import CarrouselEmbla from "./CarrouselEmbla.jsx";
-
+import video from "../../assets/letshost.mp4";
 const theme = createTheme();
 
 const Accueil = () => {
-  // const images = [image1, image2, image3];
-  // const [currentIndex, setCurrentIndex] = useState(0);
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  //   }, 7000);
-  //   return () => clearInterval(interval);
-  // }, []);
-  // return (
-  //   <ThemeProvider theme={theme}>
-  //     <Navbar />
-  //     {/* <Box
-  //       sx={{
-  //         position: "relative",
-  //         width: "100%",
-  //         maxHeight: { xs: "300px", sm: "400px", md: "550px" },
-  //         overflow: "hidden",
-  //         backgroundColor: "white",
-  //       }}
-  //     >
-  //       <img
-  //         src={coverImage}
-  //         alt="Couverture"
-  //         style={{
-  //           width: "100%",
-  //           // height: "100%",
-  //           objectFit: "cover",
-  //           display: "block",
-  //         }}
-  //       />
-  // </Box> */}
-  // <Box
-  //   sx={{
-  //     position: "relative",
-  //     width: "100%",
-  //     maxHeight: { xs: "300px", sm: "400px", md: "550px" },
-  //     overflow: "hidden",
-  //     backgroundColor: "white",
-  //   }}
-  // >
-  //   {images.map((img, index) => (
-  //     <img
-  //       key={index}
-  //       src={img}
-  //       alt={`Image slider ${index + 1}`}
-  //       style={{
-  //         width: "100%",
-  //         objectFit: "cover",
-  //         position: "absolute",
-  //         top: 0,
-  //         left: 0,
-  //         opacity: currentIndex === index ? 1 : 0,
-  //         transition: "opacity 1s ease-in-out",
-  //         zIndex: currentIndex === index ? 1 : 0,
-  //       }}
-  //     />
-  //   ))}
-  // </Box>
-  // const images = [image1, image2, image3];
-  // const [current, setCurrent] = useState(0);
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrent((current + 1) % images.length);
-  //   }, 3500);
-  //   return () => clearInterval(interval);
-  // }, [current]);
-
   return (
     <ThemeProvider theme={theme}>
       <Navbar />
-      {/* <Box
+      <Box
         sx={{
           position: "relative",
           width: "100%",
-          height: { xs: "300px", sm: "400px", md: "550px" },
+          maxHeight: { xs: "300px", sm: "400px", md: "550px" },
           overflow: "hidden",
-          backgroundColor: "white",
+          backgroundColor: "black",
         }}
       >
-      </Box> */}
-      <CarrouselEmbla />
-
-      {/* <Box
-      sx={{
-        position: "relative",
-        width: "100%",
-        height: { xs: "300px", sm: "400px", md: "550px" },
-        overflow: "hidden",
-        backgroundColor: "white",
-      }}
-    >
-      <AnimatePresence mode="wait">
-        <motion.img
-          key={images[current]}
-          src={images[current]}
-          alt={`Slide ${current + 1}`}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 1 }}
-          className="carousel-image"
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
           style={{
             width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            position: "absolute",
-            top: 0,
-            left: 0,
+            objectFit: "contain",
+            display: "block",
+            // height: "auto",
           }}
-        />
-      </AnimatePresence>
-    </Box> */}
+        >
+          <source src={video} type="video/mp4" />
+          Votre navigateur ne supporte pas la lecture vidéo.
+        </video>
+      </Box>
 
       <section className="conciergerie-section">
         <Helmet>
